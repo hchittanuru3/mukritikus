bfc719125fa8424f1b185b38b2545d0bfd38db0e
hchittanuru3 <hemanthc98@outlook.com>
2020-02-22 05:38:04 -0500

    Initial Flask

---
+++ main.py
@@ -0,0 +1,11 @@
+import flask
+
+APP = flask.Flask(__name__)
+
+@APP.route('/')
+def index():
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run()
